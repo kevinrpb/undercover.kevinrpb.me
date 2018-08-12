7fa947a864bef5643616d0372339afc03bd16c1f
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2018-08-12 15:41:52 +0200

    Initial commit

---
+++ src/js/localization.js
@@ -0,0 +1,33 @@
+const localization = {
+    'es_ES': {
+        'title': 'Infiltrado',
+        'back': '<',
+        'player': 'Jugador',
+        'decide': 'Encuentra los infiltrados',
+        'search': 'Buscar infiltrado'
+    }
+}
+
+function checkLanguage() {
+    // Check query
+    let lan_query = getParameterByName('lan')
+    if (lan_query === null || lan_query === "" || !localization[lan_query]) {
+        // Check storage
+        let lan_stored = localStorage.getItem('kUndercover_language')
+
+        if (lan_stored && lan_stored != "") {
+            location.href += '?lan=' + lan_stored
+        }
+        
+        // Set default
+        location.href += '?lan=es_ES'
+    }
+
+    localStorage.setItem('kUndercover_language', lan_query)
+    return localization[lan_query]
+}
+
+function localize(language) {
+    $('#title').text(language['title'])
+    $('#back-btn').text(language['back'])
+}
---
+++ src/js/main.js
@@ -0,0 +1,164 @@
+let language;
+
+let players;
+let undercover;
+let keyword;
+let turns = [];
+
+let currentTurn;
+
+$(function() {
+    $('#back-btn').on('click', function(e) {
+        $('#wrapper').removeClass('playing');
+    });
+
+    $('#players-less').on('click', function(e) {
+        let p = $('#players-count').text();
+        $('#players-count').text(Math.max(--p, 1));
+    });
+    $('#players-more').on('click', function(e) {
+        let p = $('#players-count').text();
+        $('#players-count').text(Math.max(++p, 1));
+    });
+
+    $('#undercover-less').on('click', function(e) {
+        let c = $('#undercover-count').text();
+        let p = $('#players-count').text();
+        $('#undercover-count').text(Math.min(Math.max(--c, 1), p-1));
+    });
+
+    $('#undercover-more').on('click', function(e) {
+        let c = $('#undercover-count').text();
+        let p = $('#players-count').text();
+        $('#undercover-count').text(Math.min(Math.max(++c, 1), Math.floor(p/2)));
+    });
+
+    $('#play-btn').on('click', function(e) {
+        setupGame();
+        startGame();
+    });
+
+    $('#toggle-word').on('click', function(e) {
+        $('#current-word .text').toggleClass('show');
+    });
+
+    $('#next-turn').on('click', function(e) {
+        makeTurn();
+    });
+
+    language = checkLanguage();
+    localize(language);
+})
+
+function setupGame() {
+    // Get Values
+    players = $('#players-count').text();
+    undercover = $('#undercover-count').text();
+    keyword = '__keyword__';
+    turns = [];
+
+    // Set turns
+    let player_str = language['player'];
+
+    for (let i = 1; i <= players; i++) {
+        if (i == players) {
+            turns.push({
+                player: player_str + ' ' + i,
+                under: false,
+                word: keyword,
+                next: language['search']
+            });
+        } else {
+            turns.push({
+                player: player_str + ' ' + i,
+                under: false,
+                word: keyword,
+                next: player_str + ' ' + (i + 1)
+            });
+        }
+    }
+
+    // Set undercovers
+    let under_indexes = [];
+
+    for (let i = 0; i < undercover; i++) {
+        let numberIsInArray = false;
+        let rand = generateRandomInt(0, players);
+
+        for(let j = 0; j < under_indexes.length; j++){
+            if(rand === under_indexes[j]) {
+                numberIsInArray = true;
+                i--;
+            }
+        }
+
+        if(!numberIsInArray){
+            under_indexes.push(rand);
+        }
+    }
+    console.log(under_indexes)
+
+    for (let i of under_indexes) {
+        turns[i].under = true;
+        turns[i].word = language['title'];
+    }
+
+    // Add decide turn
+    turns.push({
+        player: language['decide'] + ' (' + undercover + ')',
+        under: false,
+        word: '',
+        next: 'Terminar'
+    });
+
+    // Add solution
+    let str = 'Solution: ' + turns[under_indexes[0]].player;
+    for (let i = 1; i < under_indexes.length; i++) {
+        str += ', ' + turns[under_indexes[i]].player;
+    }
+    turns.push({
+        player: str,
+        under: false,
+        word: '',
+        next: 'Repetir'
+    });
+}
+
+function startGame() {
+    currentTurn = 0;
+    makeTurn();
+    $('#wrapper').addClass('playing');
+}
+
+function makeTurn() {
+    if (currentTurn == turns.length) {
+        setupGame();
+        currentTurn = 0;
+    }
+
+    $('#current-word .text').text('');
+    $('#current-word .text').removeClass('show');
+
+    let current = turns[currentTurn];
+    if (currentTurn === turns.length - 1 || currentTurn === turns.length - 2) {
+        // Solution
+        $('#current-word').hide();
+    } else {
+        $('#current-word').show();
+    }
+
+    let t = 400;
+    if (currentTurn === 0)
+        t = 20
+    
+    $('#play').animate({ opacity: 0 }, t, function() {
+        $('#current-player').text(current['player']);
+        $('#current-word .text').text(current['word']);
+        $('#next-turn').text(current['next']);
+    });
+
+    setTimeout(function() {
+        $('#play').animate({ opacity: 1 }, 400);
+    }, t);
+    currentTurn++;
+}
